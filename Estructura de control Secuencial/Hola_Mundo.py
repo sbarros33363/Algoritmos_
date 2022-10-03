20330e412691f6927da80de73aa3fe580c3b4287
Santiago Barros <39437878+sbarros33363@users.noreply.github.com>
2022-10-03 12:32:43 -0500

    Create Hola_Mundo.py

---
+++ Estructura de control Secuencial/Hola_Mundo.py
@@ -0,0 +1,15 @@
+#ENTRADA1
+
+"""
+Comentario
+Muchas 
+Lineas
+"""
+
+edad_uno=int(input("Ingrese edad uno:"))
+edad_dos=int(input("Ingrese edad dos:"))
+edad_tres=int(input("Ingrese edad tres:"))
+
+promedio=(edad_uno + edad_dos + edad_tres)/3
+
+print("Su promedio es: ", promedio)
