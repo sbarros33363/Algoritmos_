2e4ed277775a55dc9cd3ac76ae1d4128aa5d7887
Santiago Barros <39437878+sbarros33363@users.noreply.github.com>
2022-10-21 09:46:17 -0500

    a

---
+++ Estructura de control Secuencial/PiesMetros.py
@@ -0,0 +1,7 @@
+metros=float(input("Ingrese una cantidad en metros: "))
+#caja negra
+pulgadas=metros*39.37
+pies=metros*3.281
+#salidas
+print("Metros convertidos a pulgadas es: ",pulgadas)
+print("Metros convetidos a pies es: ",pies)
---
+++ Estructura de control Secuencial/Porcentaje.py
@@ -0,0 +1,9 @@
+
+#entradas
+mujeres=int(input("Ingrese el nuemro de mujeres: "))
+hombres=int(input("Ingrese el numero de hombres: "))
+#caja negra
+total=(mujeres+hombres)
+#salidas
+print("El porcentaje de mujeres es: ",(mujeres/total)*100,"%")
+print("El porcentaje de hombres es: ",(hombres/total)*100,"%")
